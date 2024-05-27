c98dc4e84f8fd5069dd90b300114d030515cb018
Aarush Narayan Banerjee <aanbaner@ucsc.edu>
2024-05-27 03:35:11 -0700

    changing FreeRunningTimer.c/h timer to TMR3 instead of TMR5

--- ECE118 Final Project.X/FreeRunningTimer.c
+++ ECE118 Final Project.X/FreeRunningTimer.c
@@ -25,18 +25,18 @@ static unsigned int milliseconds;
  * @brief  Initializes the timer module */
 void FreeRunningTimer_Init(void) {
     
-    T5CON = 0; // turn off T, select internal clk, 1:1 pre scalar
+    T3CON = 0; // turn off T, select internal clk, 1:1 pre scalar
     
     // Period Register
-    PR5 = 40001; // PRx = 40MHz/(desired * PreScalar) + 1
+    PR3 = 40001; // PRx = 40MHz/(desired * PreScalar) + 1
     
     // interrupts
-    IFS0bits.T5IF = 0; // clear the flag
-    IPC5bits.T5IP = 2; // priority bit
-    IPC5bits.T5IS = 0; // subpriority bit
-    IEC0bits.T5IE = 1; // enable interrupts
+    IFS0bits.T3IF = 0; // clear the flag
+    IPC3bits.T3IP = 2; // priority bit
+    IPC3bits.T3IS = 0; // subpriority bit
+    IEC0bits.T3IE = 1; // enable interrupts
     
-    T5CONbits.ON = 1; // turn on 
+    T3CONbits.ON = 1; // turn on 
     
 }
 
@@ -52,13 +52,13 @@ unsigned int FreeRunningTimer_GetMilliSeconds(void) {return milliseconds;}
  * @param None
  * @return the current MicroSecond Count
    */
-unsigned int FreeRunningTimer_GetMicroSeconds(void) {return (microseconds + (TMR5 / TICKS_PER_SECOND));}
+unsigned int FreeRunningTimer_GetMicroSeconds(void) {return (microseconds + (TMR3 / TICKS_PER_SECOND));}
 
 #ifdef FRT_INTERRUPT
-void __ISR(_TIMER_5_VECTOR) Timer5IntHandler(void) {
+void __ISR(_TIMER_3_VECTOR) Timer3IntHandler(void) {
     
-    if (IFS0bits.T5IF) {
-        IFS0bits.T5IF = 0; // clear the flag
+    if (IFS0bits.T3IF) {
+        IFS0bits.T3IF = 0; // clear the flag
         
         LEDS_SET(LEDS_GET() ^ 0x10);
         
@@ -73,4 +73,4 @@ void __ISR(_TIMER_5_VECTOR) Timer5IntHandler(void) {
     }
     
 }
-#endif FRT_INTERRUPT
+#endif
---
+++ ECE118 Final Project.X/build/default/production/FreeRunningTimer.o.d
@@ -0,0 +1,6 @@
+build/default/production/FreeRunningTimer.o: FreeRunningTimer.c \
+ FreeRunningTimer.h BOARD.h
+
+FreeRunningTimer.h:
+
+BOARD.h:
